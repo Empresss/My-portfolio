c5286ce0d350dcf7c2cf608167164e784a3d0c18
Onyinye <onyiodim@gmail.com>
2022-07-09 06:45:18 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 
     $(function () {
         element.typed({
-            strings: ["Web Designer.", "Graphic Designer."],
+            strings: ["Data Analyst.", "Frontend Developer"],
             typeSpeed: 100,
             loop: true,
             autoplay: true,
